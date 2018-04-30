76768c16dd5634b7000c90e524ccaf4350cd28af
andremartingo <andre.martingo@gmail.com>
2018-04-30 19:09:50 +0100

    implement burguer menu

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
-import Navbar from "./components/Navbar";
+import Navbar from "./container/Navbar";
 import Intro from "./components/Intro";
 import Footer from "./components/Footer";
 
